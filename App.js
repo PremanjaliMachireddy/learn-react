737ec2da526d0c412eecc8c95d19f923c0b7ddc9
prema <anjali.machireddy@gmail.com>
2025-05-24 16:03:03 -0400

    lession2-parcel

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/* <div id="parent">
-<div id="child">
-<h1>I'm an H1 tag</h1>
-<h2>I'm an H2 tag>/h2>
-</div>
-<div id="child2">
-<h1>I'm an H1 tag</h1>
-<h2>I'm an H2 tag>/h2>
-</div>
-</div> */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
@@ -19,12 +11,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "i'm and h2 tag"),
   ]),
 ]);
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello world from React"
-// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-#Learning react
+# Learning react
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Faster builds becuase it is doing caching
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - to support order browsers
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
